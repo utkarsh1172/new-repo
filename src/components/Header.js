7b170a93394da9d0f585aefbf218cceb54278e9c
Utkarsh Adhikrao Kamble <utkarsh1172@gmail.com>
2022-09-25 11:01:21 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import './assets/scss/main.scss'
+
+
+function header() {
+  return (
+ <header>
+    <h1>The <strong>Anime</strong>Database</h1>
+ </header>
+  )
+}
+
+export default header
